370525c54a5d7c1d7b8370a373f4bd633e86341d
moriartyy <moriartyy@github.com>
2017-05-07 00:18:09 +0800

    hello

--- learnj-nio/src/main/java/org/learnj/nio/tcp/Boot.java
+++ learnj-nio/src/main/java/org/learnj/nio/tcp/Boot.java
@@ -1,11 +1,20 @@
 package org.learnj.nio.tcp;
 
+import java.io.IOException;
+import java.net.Socket;
+
 /**
  * @author hongmiao.yu on 2016/8/9.
  */
 public class Boot {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        Socket socket = new Socket("", 8080);
+        socket.getOutputStream();
+
+
+
         byte a = (byte)0x80;
         byte b = ~0x7f;
         System.out.println(a == b);

b543f97ff8df947de2ab9f7dec5e30910102803c
hongmiao.yu <hongmiao.yu@mtime.com>
2017-05-08 10:55:20 +0800

    Add dubbo

--- learnj-lang/src/main/java/org/learnj/lang/serviceloader/Bootstrap.java
+++ learnj-lang/src/main/java/org/learnj/lang/serviceloader/Bootstrap.java
@@ -1,8 +1,6 @@
 package org.learnj.lang.serviceloader;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.ServiceLoader;
 
 /**
  * @author hongmiao.yu on 2016/8/1.
@@ -10,40 +8,9 @@ import java.util.Set;
 public class Bootstrap {
 
     public static void main(String[] args) {
-
-        String s = new String(Character.toChars(0x2F81A));
-        System.out.println(s.length());
-        System.out.println(Character.isHighSurrogate(s.charAt(0)));
-        System.out.format("%X", s.codePointAt(0));
-
-
-//        ServiceLoader<PrintService> loader = ServiceLoader.load(PrintService.class);
-//        for (PrintService printService : loader) {
-//            printService.print();
-//        }
-    }
-
-    static Map<Set<String>, Integer> docs;
-    static void search(Set<String> keywords) {
-        for (Set<String> doc : docs.keySet()) {
-            for (String keyword : keywords) {
-                if (!contains(doc, keyword)) {
-                    break;
-                }
-                System.out.println(docs.get(doc));
-            }
+        ServiceLoader<PrintService> loader = ServiceLoader.load(PrintService.class);
+        for (PrintService printService : loader) {
+            printService.print();
         }
     }
-
-    private static boolean contains(Set<String> doc, String keyword) {
-        for (String s : doc) {
-            if (s.equalsIgnoreCase(keyword)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-
-
 }
